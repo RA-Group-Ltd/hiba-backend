f9e4703da787b10fe30c6f15fb28db46ccf0212c
Batyrkhan-afk <mr.alibi0898@bk.ru>
2024-03-14 13:28:31 +0500

    (fix): Fixed in NotificationServiceImpl message

--- src/main/java/kz/wave/hiba/Service/Impl/NotificationServiceImpl.java
+++ src/main/java/kz/wave/hiba/Service/Impl/NotificationServiceImpl.java
@@ -20,11 +20,11 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     public void sendNotificationToUser(Order order) {
         User user = order.getUser();
-        String message = "Ваш заказ передан в службу доставки " + "Ваш заказ будет доставлен " +  +  ". Курьер с вами заранее свяжется";
+//        String message = "Ваш заказ передан в службу доставки " + "Ваш заказ будет доставлен " +  +  ". Курьер с вами заранее свяжется";
 
         Notification notification = new Notification();
         notification.setTime(Instant.now());
-        notification.setMessage(message);
+//        notification.setMessage(message);
         notification.setUser(user);
 //        notification.setNotificationCategory(NotificationCategory.);
 
